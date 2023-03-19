41e8c560455209ed1f9030fe738600fdf5195d61
smolianko2503 <smolianko.andrey1993@gmail.com>
2023-03-19 21:38:32 +0200

    add localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,78 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { Section } from './Section/Section';
+import { Phonebook } from './Phonebook/Phonebook';
+import { ContactsList } from './ContactsList/ContactsList';
+import { Filter } from './Filter/Filter';
+import { GlobalStyle } from './GlobalStyle';
+import { Container } from './App.styled';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  componentDidMount() {
+    const savedContacts = localStorage.getItem('contacts');
+    if (savedContacts !== null) {
+      const parsedContacts = JSON.parse(savedContacts);
+      this.setState({ contacts: parsedContacts });
+      return;
+    }
+
+    this.setState({ contacts: [] });
+  }
+
+  componentDidUpdate(_, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
+  addContact = newContact => {
+    if (
+      this.state.contacts.some(
+        contact => contact.name.toLowerCase() === newContact.name.toLowerCase()
+      )
+    ) {
+      alert(`${newContact.name} is already in contacts`);
+    } else {
+      this.setState(prevState => {
+        return {
+          contacts: [...prevState.contacts, newContact],
+        };
+      });
+    }
+  };
+
+  searchContact = evt => {
+    this.setState({ filter: evt.target.value });
+  };
+
+  deleteContact = contactId => {
+    this.setState(prevState => {
+      return {
+        contacts: prevState.contacts.filter(
+          contact => contact.id !== contactId
+        ),
+      };
+    });
+  };
+
+  render() {
+    return (
+      <Container>
+        <Section title={'Phonebook'} />
+        <Phonebook onAddContact={this.addContact} filter={this.state.filter} />
+        <Section title={'Contacts'} />
+        <Filter filter={this.state.filter} onSearch={this.searchContact} />
+        <ContactsList
+          contacts={this.state.contacts}
+          filter={this.state.filter}
+          onDelete={this.deleteContact}
+        />
+        <GlobalStyle />
+      </Container>
+    );
+  }
+}
